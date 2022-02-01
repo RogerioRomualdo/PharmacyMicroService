d65e703cc3ab53ad152a34f46d8a518ef83e6d96
Rogério Romualdo <rogerioromualdo12@gmail.com>
2022-02-01 20:30:06 -0300

    Feat: Added a general validator

---
+++ src/utils/validator.ts
@@ -0,0 +1,23 @@
+export class Validator {
+  ValidateFalsyFields<T extends Object>(
+    data: T,
+    exceptions: Array<keyof T> = []
+  ) {
+    const ParamsSplitIntoEntries: Array<Array<keyof T>> = Object.entries(data);
+
+    const falsyFields = ParamsSplitIntoEntries.filter(([key, value]) =>
+      exceptions.includes(key) ? false : !value
+    ).map(([key]) => key);
+
+    console.log(exceptions);
+    console.log(falsyFields);
+
+    if (falsyFields.length === 0) return null;
+
+    const errorMessage =
+      falsyFields.join(", ") +
+      (falsyFields.length === 1 ? " is a empty field" : " are empty fields");
+
+    return new Error(errorMessage);
+  }
+}
