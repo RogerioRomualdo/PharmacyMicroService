a20e0a480c5e8a148acc816c06de649bef908e30
RogerioRomualdo <rogerioromualdo12@gmail.com>
2022-02-04 01:36:06 -0300

    Feat: Add pharmacyProducts repository

---
+++ src/core/repositories/pharmacyProducts/PharmacyProducts.ts
@@ -0,0 +1,27 @@
+import { getRepository, Repository } from "typeorm";
+import { Pharmacy } from "../../../infra/database/models/Pharmacy";
+import { PharmacyProducts } from "../../../infra/database/models/PharmacyProducts";
+import { IPharmacyProductsRepository } from "./IPharmacyProducts";
+
+export class PharmacyProductsRepository implements IPharmacyProductsRepository {
+  private typeOrm: Repository<PharmacyProducts>;
+
+  constructor() {
+    this.typeOrm = getRepository(PharmacyProducts);
+  }
+
+  linkProductsToPharmacy = async (
+    pharmacy: Pharmacy,
+    productIds: Array<string>
+  ) => {
+    await Promise.all(
+      productIds.map((productId) => {
+        this.typeOrm.save({ pharmacy, productId, unitsInStock: 0 });
+      })
+    );
+  };
+
+  getAllProductsLinkedToPharmacy = async (pharmacy: Pharmacy) => {
+    return await this.typeOrm.find({ where: { pharmacy } });
+  };
+}
